878a843353bc100f0f0bfd2e53b4a758191730c2
Girech <88850410+Girech@users.noreply.github.com>
2021-11-24 22:09:29 -0300

    update lista

--- Ex6.py
+++ Ex6.py
@@ -1,7 +1,4 @@
-def multi(a,b):
-    r = a*b
-    return r
+f = lambda  a, b : a*b
 n1 = int(input("Digite o valor de A: "))
 n2 = int(input("Digite o valor de B: "))
-func = multi(n1,n2)
-print(f"{n1}*{n2}= {func}")
+print(f(n1,n2))
--- Ex7.py
+++ Ex7.py
@@ -1,24 +1,23 @@
-def media(a,b,c,d):
-    if((a>=0 and a<=4)or(c>=0 and c<=4)):
-        if((b>=0 and b<=6)or(d>=0 and d<=6)):
-            r = (a+b)
-            return r
-        else:
-            print("Digite um valor valido")
-    else:
-        print("Digite um valor valido")
+func = lambda a, b, c, d: ((a+b+c+d)/2)
 
 n1 = int(input("Digite a primeira nota: "))
 n2 = int(input("Digite a segunda nota: "))
 n3 = int(input("Digite a terceira nota: "))
 n4 = int(input("Digite a quarta nota: "))
-func = media(n1,n2,n3,n4)
-if (func>=0 and func<=4.9):
-    print(f"Media: {func} Nota: D")
-if (func>=5 and func<=6.9):
-    print(f"Media: {func} Nota: C")
-if (func>=7 and func<=8.9):
-    print(f"Media: {func} Nota: B")
-if (func>=9 and func<=10):
-    print(f"Media: {func} Nota: A")
-        
+
+if((n1>=0 and n1<=4)or(n3>=0 and n3<=4)):
+    if((n2>=0 and n2<=6)or(n4>=0 and n4<=6)):
+        print(func(n1,n2,n3,n4))
+    else:
+        print("Digite um valor valido")
+else:
+    print("Digite um valor valido")
+x = func(n1,n2,n3,n4)
+if (x>=0 and x<=4.9):
+    print(f"Media: {x} Nota: D")
+if (x>=5 and x<=6.9):
+    print(f"Media: {x} Nota: C")
+if (x>=7 and x<=8.9):
+    print(f"Media: {x} Nota: B")
+if (x>=9 and x<=10):
+    print(f"Media: {x} Nota: A")
